63f0008d60ece9ac004ff75c462813201bd91979
chowy1026 <chowy1026@gmail.com>
2017-04-05 10:40:49 +0800

    modified author, README

--- tumblr_scraper/__init__.py
+++ tumblr_scraper/__init__.py
@@ -1,10 +1,10 @@
 """
 tumblr-scraper
-:copyright: (c) 2017 by Tristan Waddington.
+:copyright: (c) 2017 by cHoWy1026.
 :license: Apache 2.0, see LICENSE for more details.
 """
 
-__version__ = '1.0.0'
-__author__ = 'Tristan Waddington'
+__version__ = '1.1.0'
+__author__ = 'cHoWy1026'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2017 Tristan Waddington'
+__copyright__ = 'Copyright 2017 cHoWy1026'
